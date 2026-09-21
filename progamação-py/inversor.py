d2e1d00a6b2b568a7c0787ac1d4d32730cd45e6c
Artur Berredo <arberredo@gmail.com>
2026-09-21 11:43:35 -0300

    Inversor

---
+++ progamação-py/inversor.py
@@ -0,0 +1,8 @@
+def inverter_texto (texto):
+  return texto [texto::-1]
+def inverte_lista(lista):
+  return lista [::-1]
+if_name_ == "__main__":
+msg = "Python"
+print(f"Original: {msg} | Invertido: {inverter_texto(msg)}")
+                    
